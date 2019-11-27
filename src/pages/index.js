54ca1daad20bcfd2b418d1d5285a434afe024fe0
Tacit <jeremiah.fallin@gmail.com>
2019-11-26 20:18:52 -0800

    ??

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,118 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, graphql } from "gatsby";
 import Img from "gatsby-image";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <div
-      style={{
-        display: "grid",
-        gridTemplateRows: "1fr 4fr",
-        gridRowGap: "30px",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <header
+const IndexPage = ({ data }) => {
+  const [imageIndex, setImageIndex] = useState(5);
+
+  useEffect(() => {
+    data.allFile.edges.shift();
+    setImageIndex(i => i - 1);
+  }, []);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div
         style={{
-          alignSelf: "end",
-          justifySelf: "center",
+          display: "grid",
+          gridTemplateRows: "1fr 4fr 0fr",
+          gridRowGap: "30px",
+          width: "100%",
+          height: "100vh",
         }}
       >
-        <h1
+        <header
           style={{
-            marginBottom: 0,
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
           }}
         >
-          Home
-        </h1>
-      </header>
-      <div>
-        <div style={{ gridRow: "span 2" }}>
-          <div
+          <h1
+            onClick={e => {
+              setImageIndex(i =>
+                i - 1 >= 0 ? i - 1 : data.allFile.edges.length - 1
+              );
+            }}
             style={{
-              display: "grid",
-              gridTemplateColumns: "repeat(auto-fill, minmax(200px, 2fr))",
-              gridAutoRows: "minmax(100px, 200px)",
-              gridGap: "20px",
-              width: "100%",
-              height: "100%",
+              marginBottom: 0,
+              alignSelf: "end",
+              justifySelf: "left",
+              color: "#6699ff",
+              cursor: "pointer",
             }}
           >
-            {data.allFile.edges.map(image => {
-              return (
-                <React.Fragment key={image.node.relativeDirectory}>
-                  {image.node.relativeDirectory && (
-                    <Link
-                      to={`/${image.node.relativeDirectory}/`}
-                      style={{ boxShadow: `none` }}
-                    >
-                      <div className="item">
-                        <Img
-                          fluid={image.node.childImageSharp.fluid}
-                          style={{ height: "100%", width: "100%" }}
-                        />
-                        <div className="item__overlay">
-                          <button>{image.node.relativeDirectory}</button>
-                        </div>
+            ← Previous
+          </h1>
+          <h1
+            style={{
+              marginBottom: 0,
+              alignSelf: "end",
+              justifySelf: "center",
+            }}
+          >
+            Home
+          </h1>
+          <h1
+            onClick={e =>
+              setImageIndex(i => (i + 1) % data.allFile.edges.length)
+            }
+            style={{
+              marginBottom: 0,
+              alignSelf: "end",
+              justifySelf: "right",
+              color: "#6699ff",
+              cursor: "pointer",
+            }}
+          >
+            Next →
+          </h1>
+        </header>
+        <div>
+          <div style={{ gridRow: "span 2" }}>
+            <div
+              style={{
+                display: "grid",
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <React.Fragment key={imageIndex}>
+                {data.allFile.edges[imageIndex].node.relativeDirectory && (
+                  <Link
+                    to={`/${data.allFile.edges[imageIndex].node.relativeDirectory}/`}
+                    style={{ boxShadow: `none` }}
+                  >
+                    <div className="item">
+                      <Img
+                        fluid={
+                          data.allFile.edges[imageIndex].node.childImageSharp
+                            .fluid
+                        }
+                        style={{ height: "100%", width: "100%" }}
+                      />
+                      <div className="item__overlay">
+                        <button>
+                          {
+                            data.allFile.edges[imageIndex].node
+                              .relativeDirectory
+                          }
+                        </button>
                       </div>
-                    </Link>
-                  )}
-                </React.Fragment>
-              );
-            })}
+                    </div>
+                  </Link>
+                )}
+              </React.Fragment>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default IndexPage;
 
