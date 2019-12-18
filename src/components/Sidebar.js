dacf1b5b94285d2523824f5e7b43a307fbb5279e
Tacit <jeremiah.fallin@gmail.com>
2019-12-17 19:58:13 -0800

    Fixed css issue on initial load.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,28 +4,27 @@ import styled from "styled-components";
 
 import Socials from "./Socials";
 
-import useWindowWidth from "../components/hooks/useWindowWidth";
-
-const SidebarWebStyle = styled.div`
+const SidebarStyle = styled.div`
   display: grid;
   width: 100%;
-  grid-column-gap: 30px;
-  grid-template-rows: 1fr 3fr 5fr;
-  height: 100vh;
-  grid-row-gap: 30px;
-  grid-template-columns: 2fr 2fr;
-`;
 
-const SidebarPhoneStyle = styled.div`
-  display: grid;
-  width: 100%;
-  grid-template-columns: 1fr 1fr 1fr;
-  height: 100%;
-  grid-auto-flow: column;
-  align-self: center;
-  justify-self: center;
-  padding-top: 1em;
-  padding-left: 1em;
+  @media only screen and (max-width: 900px) {
+    grid-template-columns: 1fr 1fr 1fr;
+    height: 100%;
+    grid-auto-flow: column;
+    align-self: center;
+    justify-self: center;
+    padding-top: 1em;
+    padding-left: 1em;
+  }
+
+  @media only screen and (min-width: 900px) {
+    grid-column-gap: 30px;
+    grid-template-rows: 1fr 3fr 5fr;
+    height: 100vh;
+    grid-row-gap: 30px;
+    grid-template-columns: 2fr 2fr;
+  }
 `;
 
 const MenuStyles = styled.div`
@@ -36,7 +35,6 @@ const MenuStyles = styled.div`
 
   @media only screen and (min-width: 900px) {
     grid-column: -2 / -1;
-    grid-template-rows: repeat(3, 1fr);
     align-self: center;
   }
 `;
@@ -58,6 +56,9 @@ const StyledList = styled.ul`
   padding: 0;
   float: left;
   padding-bottom: 0;
+  @media only screen and (max-width: 900px) {
+    display: none;
+  }
 `;
 
 const StyledSocials = styled.div`
@@ -72,85 +73,62 @@ const StyledSocials = styled.div`
 const StyledLinks = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
+  @media only screen and (min-width: 900px) {
+    display: none;
+  }
 `;
 
 const Sidebar = () => {
-  const width = useWindowWidth();
-  console.log(width);
-
-  if (width < 900) {
-    return (
-      <SidebarPhoneStyle>
-        <StyledName>
-          <Link to="/">
-            <strong>Dan Scott</strong>
+  return (
+    <SidebarStyle>
+      <StyledName>
+        <Link to="/">
+          <strong>Dan Scott</strong>
+        </Link>
+      </StyledName>
+      <MenuStyles>
+        <StyledLinks>
+          <Link to="/" activeStyle={{ textDecoration: "underline" }}>
+            Home
           </Link>
-        </StyledName>
-        <MenuStyles>
-          <StyledLinks>
+          <Link
+            to="/gallery"
+            activeStyle={{ textDecoration: "underline" }}
+            style={{ justifySelf: "center", alignSelf: "center" }}
+          >
+            Galleries
+          </Link>
+          <Link
+            to="/about"
+            activeStyle={{ textDecoration: "underline" }}
+            style={{ justifySelf: "end", alignSelf: "end" }}
+          >
+            About
+          </Link>
+        </StyledLinks>
+        <StyledList>
+          <li>
             <Link to="/" activeStyle={{ textDecoration: "underline" }}>
               Home
             </Link>
-            <Link
-              to="/gallery"
-              activeStyle={{ textDecoration: "underline" }}
-              style={{ justifySelf: "center", alignSelf: "center" }}
-            >
+          </li>
+          <li>
+            <Link to="/gallery" activeStyle={{ textDecoration: "underline" }}>
               Galleries
             </Link>
-            <Link
-              to="/about"
-              activeStyle={{ textDecoration: "underline" }}
-              style={{ justifySelf: "end", alignSelf: "end" }}
-            >
+          </li>
+          <li>
+            <Link to="/about" activeStyle={{ textDecoration: "underline" }}>
               About
             </Link>
-          </StyledLinks>
-        </MenuStyles>
-        <StyledSocials>
-          <Socials />
-        </StyledSocials>
-      </SidebarPhoneStyle>
-    );
-  } else {
-    return (
-      <SidebarWebStyle>
-        <StyledName>
-          <Link to="/">
-            <strong>Dan Scott</strong>
-          </Link>
-        </StyledName>
-        <div
-          style={{
-            gridColumn: "-2/-1",
-            gridTemplateRows: "repeat(3, 1fr)",
-            alignSelf: "center",
-          }}
-        >
-          <StyledList>
-            <li>
-              <Link to="/" activeStyle={{ textDecoration: "underline" }}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/gallery" activeStyle={{ textDecoration: "underline" }}>
-                Galleries
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" activeStyle={{ textDecoration: "underline" }}>
-                About
-              </Link>
-            </li>
-          </StyledList>
-        </div>
-        <div style={{ gridColumn: "-2/-1" }}>
-          <Socials />
-        </div>
-      </SidebarWebStyle>
-    );
-  }
+          </li>
+        </StyledList>
+      </MenuStyles>
+      <StyledSocials>
+        <Socials />
+      </StyledSocials>
+    </SidebarStyle>
+  );
 };
 
 export default Sidebar;
